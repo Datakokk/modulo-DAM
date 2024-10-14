b1fd1393beddab5f621cca640c714034f46e1435
Datakokk <fiodorelzarster@gmx.com>
2024-10-14 21:01:23 +0200

    método ImprimirMensajeConRetraso implementado

--- PSP/ProgramacionMultiproceso/src/hilos/Trabajador.java
+++ PSP/ProgramacionMultiproceso/src/hilos/Trabajador.java
@@ -8,20 +8,34 @@ public class Trabajador extends Thread {
 	
 	public Trabajador(String nombre, int prioridad, String mensaje) {
 		super(nombre);
+		this.mensaje = mensaje;
+		setPriority(prioridad); //Establecemos la prioridad del hiflo
+	}
+	
+	public void ImprimirMensajeConRetraso() {
+		try {
+			//Simulamos un retraso de dos segundos
+			Thread.sleep(2000);
+			
+			//Imprimir el nombre del hilo y el mensaje procesado
+			System.out.println("Ha procesado el mensaje" + mensaje);
+		}catch(InterruptedException ie) {
+			// Manejamos la excepcion en caso que el hilo se interrumpa.
+			System.out.println(getName() + "se ha interrumpido.");
+		}
 	}
 	
 	@Override
 	public void run() {
-		
+		// Llamamos el metodo para procesar el mensaje.
+		ImprimirMensajeConRetraso();
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Trabajador> trabajadores = new ArrayList<>();
 		
-		String[] mensajes = {
-				
-		};
+		String[] mensajes = {"Mensaje 1", "Mensaje 2", "Mensaje 3", "Mensaje 4", "Mensaje 5"};
 		
 	}
 
