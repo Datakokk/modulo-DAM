b53368d30c65f4f91767e10c917d3d6eed261b93
Datakokk <fiodorelzarster@gmx.com>
2024-10-15 18:39:05 +0200

    Ejercicio para casa empezado

---
+++ AD/accesoFicherosEjercicios/src/accesoFicheros/NumEmpleadoDeApellido.java
@@ -0,0 +1,14 @@
+package accesoFicheros;
+
+import java.io.File;
+
+public class NumEmpleadoDeApellido {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String path = "";
+		File fichero = new File(path + "FichPersona.txt");
+	}
+
+}
