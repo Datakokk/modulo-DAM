53e8f6c314d0947c9e5e283721f7825429991083
Datakokk <fiodorelzarster@gmx.com>
2024-10-15 20:12:49 +0200

    Se ha agregado la funcionalidad de finish()

--- PMDM/ClasesAsociadasBotones/app/src/main/java/com/example/clasesasociadasbotones/menu/Activity3.java
+++ PMDM/ClasesAsociadasBotones/app/src/main/java/com/example/clasesasociadasbotones/menu/Activity3.java
@@ -1,6 +1,7 @@
 package com.example.clasesasociadasbotones.menu;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,8 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+        findViewById(R.id.finish).setOnClickListener(v->finish());
+
     }
 }
