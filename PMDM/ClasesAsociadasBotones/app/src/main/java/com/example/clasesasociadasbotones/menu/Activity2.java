2a1340b9e45657c9536b02cd961dbfc509e53986
Datakokk <fiodorelzarster@gmx.com>
2024-10-15 17:53:26 +0200

    Activity 2 modificado Introduccion almacenamiento Android

--- PMDM/ClasesAsociadasBotones/app/src/main/java/com/example/clasesasociadasbotones/menu/Activity2.java
+++ PMDM/ClasesAsociadasBotones/app/src/main/java/com/example/clasesasociadasbotones/menu/Activity2.java
@@ -1,6 +1,10 @@
 package com.example.clasesasociadasbotones.menu;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,24 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        Button buttonSaveName = findViewById(R.id.buttonSave);
+        TextView textViewName = findViewById(R.id.TextViewName);
+
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        String savedName = sharedPreferences.getString("name", "No none saved");
+        textViewName.setText(savedName);
+
+        buttonSaveName.setOnClickListener( v -> {
+            String newName = editTextName.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("name", newName);
+                editor.apply();
+
+                textViewName.setText(newName);
+        });
     }
 }
