e6aa9ac70ca5cc9ed0b646f12f0895c54638a6bf
Datakokk <fiodorelzarster@gmx.com>
2024-10-16 21:02:04 +0200

    Ejercicios de matrices iniciado

---
+++ DI/Matices/Matices/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim num As Integer = 0
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If num <= 19 AndAlso num >= 0 Then
+            num += 1
+            txtElemento.Text = num
+        Else
+            num = 20
+        End If
+
+    End Sub
+
+    Private Sub btnRestar_Click(sender As Object, e As EventArgs) Handles btnRestar.Click
+        If num <= 20 AndAlso num >= 1 Then
+            num -= 1
+            txtElemento.Text = num
+        Else
+            num = 0
+        End If
+    End Sub
+End Class
