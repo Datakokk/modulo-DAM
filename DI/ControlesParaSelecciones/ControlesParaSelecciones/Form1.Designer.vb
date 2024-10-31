853b5bf85d9bab80ba9d67b4d30eff2a9dec647d
Datakokk <fiodorelzarster@gmx.com>
2024-10-31 23:23:19 +0100

    DI: Actividad Control para selecciones - finalizado

--- DI/ControlesParaSelecciones/ControlesParaSelecciones/Form1.Designer.vb
+++ DI/ControlesParaSelecciones/ControlesParaSelecciones/Form1.Designer.vb
@@ -23,22 +23,22 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.gbProcesador = New System.Windows.Forms.GroupBox()
-        Me.gbRAM = New System.Windows.Forms.GroupBox()
-        Me.gbDisco = New System.Windows.Forms.GroupBox()
-        Me.gbOpciones = New System.Windows.Forms.GroupBox()
-        Me.cbDVD = New System.Windows.Forms.CheckBox()
-        Me.cbAltavoces = New System.Windows.Forms.CheckBox()
-        Me.cbAudio = New System.Windows.Forms.CheckBox()
-        Me.cbImpresora = New System.Windows.Forms.CheckBox()
-        Me.rb_500GB = New System.Windows.Forms.RadioButton()
-        Me.rb_1TB = New System.Windows.Forms.RadioButton()
-        Me.rb_2TB = New System.Windows.Forms.RadioButton()
-        Me.rb_8GB = New System.Windows.Forms.RadioButton()
-        Me.rb_12GB = New System.Windows.Forms.RadioButton()
-        Me.rb_16GB = New System.Windows.Forms.RadioButton()
-        Me.rb_i3 = New System.Windows.Forms.RadioButton()
-        Me.rb_i5 = New System.Windows.Forms.RadioButton()
         Me.rb_i7 = New System.Windows.Forms.RadioButton()
+        Me.rb_i5 = New System.Windows.Forms.RadioButton()
+        Me.rb_i3 = New System.Windows.Forms.RadioButton()
+        Me.gbRAM = New System.Windows.Forms.GroupBox()
+        Me.rb_16GB = New System.Windows.Forms.RadioButton()
+        Me.rb_12GB = New System.Windows.Forms.RadioButton()
+        Me.rb_8GB = New System.Windows.Forms.RadioButton()
+        Me.gbDisco = New System.Windows.Forms.GroupBox()
+        Me.rb_2TB = New System.Windows.Forms.RadioButton()
+        Me.rb_1TB = New System.Windows.Forms.RadioButton()
+        Me.rb_500GB = New System.Windows.Forms.RadioButton()
+        Me.gbOpciones = New System.Windows.Forms.GroupBox()
+        Me.cbImpresora = New System.Windows.Forms.CheckBox()
+        Me.cbAudio = New System.Windows.Forms.CheckBox()
+        Me.cbAltavoces = New System.Windows.Forms.CheckBox()
+        Me.cbDVD = New System.Windows.Forms.CheckBox()
         Me.btnAceptar = New System.Windows.Forms.Button()
         Me.txtMostrar = New System.Windows.Forms.TextBox()
         Me.gbProcesador.SuspendLayout()
@@ -59,6 +59,39 @@ Partial Class Form1
         Me.gbProcesador.TabStop = False
         Me.gbProcesador.Text = "Procesador"
         '
+        'rb_i7
+        '
+        Me.rb_i7.AutoSize = True
+        Me.rb_i7.Location = New System.Drawing.Point(14, 129)
+        Me.rb_i7.Name = "rb_i7"
+        Me.rb_i7.Size = New System.Drawing.Size(33, 17)
+        Me.rb_i7.TabIndex = 2
+        Me.rb_i7.TabStop = True
+        Me.rb_i7.Text = "i7"
+        Me.rb_i7.UseVisualStyleBackColor = True
+        '
+        'rb_i5
+        '
+        Me.rb_i5.AutoSize = True
+        Me.rb_i5.Location = New System.Drawing.Point(14, 77)
+        Me.rb_i5.Name = "rb_i5"
+        Me.rb_i5.Size = New System.Drawing.Size(33, 17)
+        Me.rb_i5.TabIndex = 1
+        Me.rb_i5.TabStop = True
+        Me.rb_i5.Text = "i5"
+        Me.rb_i5.UseVisualStyleBackColor = True
+        '
+        'rb_i3
+        '
+        Me.rb_i3.AutoSize = True
+        Me.rb_i3.Location = New System.Drawing.Point(15, 32)
+        Me.rb_i3.Name = "rb_i3"
+        Me.rb_i3.Size = New System.Drawing.Size(33, 17)
+        Me.rb_i3.TabIndex = 0
+        Me.rb_i3.TabStop = True
+        Me.rb_i3.Text = "i3"
+        Me.rb_i3.UseVisualStyleBackColor = True
+        '
         'gbRAM
         '
         Me.gbRAM.Controls.Add(Me.rb_16GB)
@@ -71,6 +104,39 @@ Partial Class Form1
         Me.gbRAM.TabStop = False
         Me.gbRAM.Text = "RAM"
         '
+        'rb_16GB
+        '
+        Me.rb_16GB.AutoSize = True
+        Me.rb_16GB.Location = New System.Drawing.Point(16, 130)
+        Me.rb_16GB.Name = "rb_16GB"
+        Me.rb_16GB.Size = New System.Drawing.Size(52, 17)
+        Me.rb_16GB.TabIndex = 2
+        Me.rb_16GB.TabStop = True
+        Me.rb_16GB.Text = "16GB"
+        Me.rb_16GB.UseVisualStyleBackColor = True
+        '
+        'rb_12GB
+        '
+        Me.rb_12GB.AutoSize = True
+        Me.rb_12GB.Location = New System.Drawing.Point(16, 77)
+        Me.rb_12GB.Name = "rb_12GB"
+        Me.rb_12GB.Size = New System.Drawing.Size(52, 17)
+        Me.rb_12GB.TabIndex = 1
+        Me.rb_12GB.TabStop = True
+        Me.rb_12GB.Text = "12GB"
+        Me.rb_12GB.UseVisualStyleBackColor = True
+        '
+        'rb_8GB
+        '
+        Me.rb_8GB.AutoSize = True
+        Me.rb_8GB.Location = New System.Drawing.Point(16, 29)
+        Me.rb_8GB.Name = "rb_8GB"
+        Me.rb_8GB.Size = New System.Drawing.Size(46, 17)
+        Me.rb_8GB.TabIndex = 0
+        Me.rb_8GB.TabStop = True
+        Me.rb_8GB.Text = "8GB"
+        Me.rb_8GB.UseVisualStyleBackColor = True
+        '
         'gbDisco
         '
         Me.gbDisco.Controls.Add(Me.rb_2TB)
@@ -82,6 +148,39 @@ Partial Class Form1
         Me.gbDisco.TabIndex = 1
         Me.gbDisco.TabStop = False
         Me.gbDisco.Text = "Disco"
+        '
+        'rb_2TB
+        '
+        Me.rb_2TB.AutoSize = True
+        Me.rb_2TB.Location = New System.Drawing.Point(22, 129)
+        Me.rb_2TB.Name = "rb_2TB"
+        Me.rb_2TB.Size = New System.Drawing.Size(45, 17)
+        Me.rb_2TB.TabIndex = 2
+        Me.rb_2TB.TabStop = True
+        Me.rb_2TB.Text = "2TB"
+        Me.rb_2TB.UseVisualStyleBackColor = True
+        '
+        'rb_1TB
+        '
+        Me.rb_1TB.AutoSize = True
+        Me.rb_1TB.Location = New System.Drawing.Point(22, 77)
+        Me.rb_1TB.Name = "rb_1TB"
+        Me.rb_1TB.Size = New System.Drawing.Size(45, 17)
+        Me.rb_1TB.TabIndex = 1
+        Me.rb_1TB.TabStop = True
+        Me.rb_1TB.Text = "1TB"
+        Me.rb_1TB.UseVisualStyleBackColor = True
+        '
+        'rb_500GB
+        '
+        Me.rb_500GB.AutoSize = True
+        Me.rb_500GB.Location = New System.Drawing.Point(22, 29)
+        Me.rb_500GB.Name = "rb_500GB"
+        Me.rb_500GB.Size = New System.Drawing.Size(58, 17)
+        Me.rb_500GB.TabIndex = 0
+        Me.rb_500GB.TabStop = True
+        Me.rb_500GB.Text = "500GB"
+        Me.rb_500GB.UseVisualStyleBackColor = True
         '
         'gbOpciones
         '
@@ -96,25 +195,15 @@ Partial Class Form1
         Me.gbOpciones.TabStop = False
         Me.gbOpciones.Text = "Opciones"
         '
-        'cbDVD
+        'cbImpresora
         '
-        Me.cbDVD.AutoSize = True
-        Me.cbDVD.Location = New System.Drawing.Point(16, 19)
-        Me.cbDVD.Name = "cbDVD"
-        Me.cbDVD.Size = New System.Drawing.Size(49, 17)
-        Me.cbDVD.TabIndex = 0
-        Me.cbDVD.Text = "DVD"
-        Me.cbDVD.UseVisualStyleBackColor = True
-        '
-        'cbAltavoces
-        '
-        Me.cbAltavoces.AutoSize = True
-        Me.cbAltavoces.Location = New System.Drawing.Point(16, 88)
-        Me.cbAltavoces.Name = "cbAltavoces"
-        Me.cbAltavoces.Size = New System.Drawing.Size(73, 17)
-        Me.cbAltavoces.TabIndex = 1
-        Me.cbAltavoces.Text = "Altavoces"
-        Me.cbAltavoces.UseVisualStyleBackColor = True
+        Me.cbImpresora.AutoSize = True
+        Me.cbImpresora.Location = New System.Drawing.Point(16, 130)
+        Me.cbImpresora.Name = "cbImpresora"
+        Me.cbImpresora.Size = New System.Drawing.Size(72, 17)
+        Me.cbImpresora.TabIndex = 3
+        Me.cbImpresora.Text = "Impresora"
+        Me.cbImpresora.UseVisualStyleBackColor = True
         '
         'cbAudio
         '
@@ -126,114 +215,25 @@ Partial Class Form1
         Me.cbAudio.Text = "Audio"
         Me.cbAudio.UseVisualStyleBackColor = True
         '
-        'cbImpresora
+        'cbAltavoces
         '
-        Me.cbImpresora.AutoSize = True
-        Me.cbImpresora.Location = New System.Drawing.Point(16, 130)
-        Me.cbImpresora.Name = "cbImpresora"
-        Me.cbImpresora.Size = New System.Drawing.Size(72, 17)
-        Me.cbImpresora.TabIndex = 3
-        Me.cbImpresora.Text = "Impresora"
-        Me.cbImpresora.UseVisualStyleBackColor = True
+        Me.cbAltavoces.AutoSize = True
+        Me.cbAltavoces.Location = New System.Drawing.Point(16, 88)
+        Me.cbAltavoces.Name = "cbAltavoces"
+        Me.cbAltavoces.Size = New System.Drawing.Size(73, 17)
+        Me.cbAltavoces.TabIndex = 1
+        Me.cbAltavoces.Text = "Altavoces"
+        Me.cbAltavoces.UseVisualStyleBackColor = True
         '
-        'rb_500GB
+        'cbDVD
         '
-        Me.rb_500GB.AutoSize = True
-        Me.rb_500GB.Location = New System.Drawing.Point(22, 29)
-        Me.rb_500GB.Name = "rb_500GB"
-        Me.rb_500GB.Size = New System.Drawing.Size(58, 17)
-        Me.rb_500GB.TabIndex = 0
-        Me.rb_500GB.TabStop = True
-        Me.rb_500GB.Text = "500GB"
-        Me.rb_500GB.UseVisualStyleBackColor = True
-        '
-        'rb_1TB
-        '
-        Me.rb_1TB.AutoSize = True
-        Me.rb_1TB.Location = New System.Drawing.Point(22, 77)
-        Me.rb_1TB.Name = "rb_1TB"
-        Me.rb_1TB.Size = New System.Drawing.Size(45, 17)
-        Me.rb_1TB.TabIndex = 1
-        Me.rb_1TB.TabStop = True
-        Me.rb_1TB.Text = "1TB"
-        Me.rb_1TB.UseVisualStyleBackColor = True
-        '
-        'rb_2TB
-        '
-        Me.rb_2TB.AutoSize = True
-        Me.rb_2TB.Location = New System.Drawing.Point(22, 129)
-        Me.rb_2TB.Name = "rb_2TB"
-        Me.rb_2TB.Size = New System.Drawing.Size(45, 17)
-        Me.rb_2TB.TabIndex = 2
-        Me.rb_2TB.TabStop = True
-        Me.rb_2TB.Text = "2TB"
-        Me.rb_2TB.UseVisualStyleBackColor = True
-        '
-        'rb_8GB
-        '
-        Me.rb_8GB.AutoSize = True
-        Me.rb_8GB.Location = New System.Drawing.Point(16, 29)
-        Me.rb_8GB.Name = "rb_8GB"
-        Me.rb_8GB.Size = New System.Drawing.Size(46, 17)
-        Me.rb_8GB.TabIndex = 0
-        Me.rb_8GB.TabStop = True
-        Me.rb_8GB.Text = "8GB"
-        Me.rb_8GB.UseVisualStyleBackColor = True
-        '
-        'rb_12GB
-        '
-        Me.rb_12GB.AutoSize = True
-        Me.rb_12GB.Location = New System.Drawing.Point(16, 77)
-        Me.rb_12GB.Name = "rb_12GB"
-        Me.rb_12GB.Size = New System.Drawing.Size(52, 17)
-        Me.rb_12GB.TabIndex = 1
-        Me.rb_12GB.TabStop = True
-        Me.rb_12GB.Text = "12GB"
-        Me.rb_12GB.UseVisualStyleBackColor = True
-        '
-        'rb_16GB
-        '
-        Me.rb_16GB.AutoSize = True
-        Me.rb_16GB.Location = New System.Drawing.Point(16, 130)
-        Me.rb_16GB.Name = "rb_16GB"
-        Me.rb_16GB.Size = New System.Drawing.Size(52, 17)
-        Me.rb_16GB.TabIndex = 2
-        Me.rb_16GB.TabStop = True
-        Me.rb_16GB.Text = "16GB"
-        Me.rb_16GB.UseVisualStyleBackColor = True
-        '
-        'rb_i3
-        '
-        Me.rb_i3.AutoSize = True
-        Me.rb_i3.Location = New System.Drawing.Point(15, 32)
-        Me.rb_i3.Name = "rb_i3"
-        Me.rb_i3.Size = New System.Drawing.Size(33, 17)
-        Me.rb_i3.TabIndex = 0
-        Me.rb_i3.TabStop = True
-        Me.rb_i3.Text = "i3"
-        Me.rb_i3.UseVisualStyleBackColor = True
-        '
-        'rb_i5
-        '
-        Me.rb_i5.AutoSize = True
-        Me.rb_i5.Location = New System.Drawing.Point(14, 77)
-        Me.rb_i5.Name = "rb_i5"
-        Me.rb_i5.Size = New System.Drawing.Size(33, 17)
-        Me.rb_i5.TabIndex = 1
-        Me.rb_i5.TabStop = True
-        Me.rb_i5.Text = "i5"
-        Me.rb_i5.UseVisualStyleBackColor = True
-        '
-        'rb_i7
-        '
-        Me.rb_i7.AutoSize = True
-        Me.rb_i7.Location = New System.Drawing.Point(14, 129)
-        Me.rb_i7.Name = "rb_i7"
-        Me.rb_i7.Size = New System.Drawing.Size(33, 17)
-        Me.rb_i7.TabIndex = 2
-        Me.rb_i7.TabStop = True
-        Me.rb_i7.Text = "i7"
-        Me.rb_i7.UseVisualStyleBackColor = True
+        Me.cbDVD.AutoSize = True
+        Me.cbDVD.Location = New System.Drawing.Point(16, 19)
+        Me.cbDVD.Name = "cbDVD"
+        Me.cbDVD.Size = New System.Drawing.Size(49, 17)
+        Me.cbDVD.TabIndex = 0
+        Me.cbDVD.Text = "DVD"
+        Me.cbDVD.UseVisualStyleBackColor = True
         '
         'btnAceptar
         '
--- DI/ControlesParaSelecciones/ControlesParaSelecciones/Form1.vb
+++ DI/ControlesParaSelecciones/ControlesParaSelecciones/Form1.vb
@@ -1,3 +1,50 @@
 ﻿Public Class Form1
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        Dim resultado As String = ""
+        'Comprobamos los radiobutton que estan seleccionados
+        If rb_i3.Checked Then
+            resultado &= "Procesador: i3  " & vbCrLf
+        ElseIf rb_i5.Checked Then
+            resultado &= "Procesador: i5  " & vbCrLf
+        ElseIf rb_i7.Checked Then
+            resultado &= "Procesador: i7  " & vbCrLf
+        End If
 
+        If rb_8GB.Checked Then
+            resultado &= "RAM: 8GB  " & vbCrLf
+        ElseIf rb_12GB.Checked Then
+            resultado &= "RAM: 12GB  " & vbCrLf
+        ElseIf rb_16GB.Checked Then
+            resultado &= "RAM: 16GB  " & vbCrLf
+        End If
+
+        If rb_500GB.Checked Then
+            resultado &= "Disco: 500GB  " & vbCrLf
+        ElseIf rb_1TB.Checked Then
+            resultado &= "Disco: 1TB  " & vbCrLf
+        ElseIf rb_2TB.Checked Then
+            resultado &= "Disco: 2TB  " & vbCrLf
+        End If
+
+        ' Comprobamos que Checked estan seleccionados
+        If cbDVD.Checked Then
+            resultado &= "Opción: DVD  " & vbCrLf
+        End If
+
+        If cbAudio.Checked Then
+            resultado &= "Opción: Audio  " & vbCrLf
+        End If
+
+        If cbAltavoces.Checked Then
+            resultado &= "Opción: Altavoces  " & vbCrLf
+        End If
+
+        If cbImpresora.Checked Then
+            resultado &= "Opción: Impresora  " & vbCrLf
+        End If
+
+        'Mostramos el resultado
+        txtMostrar.Text = resultado
+
+    End Sub
 End Class
