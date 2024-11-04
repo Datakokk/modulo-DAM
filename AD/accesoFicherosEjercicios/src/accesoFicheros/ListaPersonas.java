6d6f9ebda0c76960b4b84318aac76bd8fa35ba55
Datakokk <fiodorelzarster@gmx.com>
2024-11-04 20:14:41 +0100

    AD: JABX project added

--- AD/accesoFicherosEjercicios/src/accesoFicheros/ListaPersonas.java
+++ AD/accesoFicherosEjercicios/src/accesoFicheros/ListaPersonas.java
@@ -2,7 +2,9 @@ package accesoFicheros;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name= "Personas")
 public class ListaPersonas {
 	private List<Persona> lista = new ArrayList<Persona>();
 	
