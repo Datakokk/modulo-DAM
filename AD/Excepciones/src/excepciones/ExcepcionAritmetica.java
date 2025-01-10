0e070719be1d8400de9ee3eb3ba8817c9e006303
Datakokk <fiodorelzarster@gmx.com>
2025-01-10 16:21:33 +0100

    AD: Se ha agregado algunos datos a la tarea

--- AD/Excepciones/src/excepciones/ExcepcionAritmetica.java
+++ AD/Excepciones/src/excepciones/ExcepcionAritmetica.java
@@ -17,12 +17,20 @@ public class ExcepcionAritmetica {
 				System.out.println("Ingrese el divisor: ");
 				int divisor = scanner.nextInt();
 				
+				int resultado = dividendo/divisor;
+				System.out.println("El resultado es: " + resultado);
+				
+				break; //Salimos de la operación si fue exitosa.
+				
 			}catch (ArithmeticException e) {
 				// TODO: handle exception
+				System.out.println("Error: No se puede dividir por cero. Por favor, ingrese nuevos números.");
 			}catch (Exception e) {
 				// TODO: handle exception
+				System.out.println("Error: Entrada no válida. Por favor, ingrese números enteros.");
+                scanner.next(); // Limpiamos la entrada que no es válida.
 			}
 		}
+		scanner.close();
 	}
-
 }
