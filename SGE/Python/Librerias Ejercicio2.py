917710210590cd6e4e2fe4561641a8afaaeb089b
Datakokk <fiodorelzarster@gmx.com>
2025-01-10 21:02:18 +0100

    SGE: Ejercicios de repaso de python

---
+++ SGE/Python/Librerias Ejercicio2.py
@@ -0,0 +1,11 @@
+nombre = input("Introduce su nombre: ")
+edad = input("Introduce su edad: ")
+direccion = input("Introduce su direccion: ")
+telefono = input("Introduce su telefono: ")
+usuario = {"nombre": nombre, "edad": edad, "direccion": direccion, "telefono": telefono}
+print(f"Tiene {edad} años, vive en {direccion} y su número de teléfono es {telefono}.")
+
+
+
+
+# tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>.
