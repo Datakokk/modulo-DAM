56f428cd5486fc72ab4f0262fbd12224b2fa7768
Datakokk <fiodorelzarster@gmx.com>
2025-01-13 19:38:13 +0100

    SGE: Ejercicio agregado de python

---
+++ SGE/Python/Librerias Ejercicio3.py
@@ -0,0 +1,10 @@
+
+
+frutas = {"Platano": 1.35, "Manzana": 0.80, "Pera": 0.85, "Naranja": 0.70 }
+
+'''fruta = input("Introduce la fruta: ")
+cantidad = input("Introduce el número de kilos: ")'''
+
+print(frutas.get("Platano"))
+
+# tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>.
