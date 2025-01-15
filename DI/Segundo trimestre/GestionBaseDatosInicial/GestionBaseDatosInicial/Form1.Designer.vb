5e763ba1cd317e8b112ec52431302b8f58853d5a
Datakokk <fiodorelzarster@gmx.com>
2025-01-15 20:51:33 +0100

    DI: Se ha agregado Gestion de Base de Datos

---
+++ DI/Segundo trimestre/GestionBaseDatosInicial/GestionBaseDatosInicial/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bntConectar = New System.Windows.Forms.Button()
+        Me.btnListar = New System.Windows.Forms.Button()
+        Me.btnAlta = New System.Windows.Forms.Button()
+        Me.btnBaja = New System.Windows.Forms.Button()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblDireccion = New System.Windows.Forms.Label()
+        Me.lblTelefono = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'bntConectar
+        '
+        Me.bntConectar.Location = New System.Drawing.Point(565, 29)
+        Me.bntConectar.Name = "bntConectar"
+        Me.bntConectar.Size = New System.Drawing.Size(75, 23)
+        Me.bntConectar.TabIndex = 0
+        Me.bntConectar.Text = "Conectar"
+        Me.bntConectar.UseVisualStyleBackColor = True
+        '
+        'btnListar
+        '
+        Me.btnListar.Location = New System.Drawing.Point(565, 79)
+        Me.btnListar.Name = "btnListar"
+        Me.btnListar.Size = New System.Drawing.Size(75, 23)
+        Me.btnListar.TabIndex = 1
+        Me.btnListar.Text = "Listar"
+        Me.btnListar.UseVisualStyleBackColor = True
+        '
+        'btnAlta
+        '
+        Me.btnAlta.Location = New System.Drawing.Point(565, 130)
+        Me.btnAlta.Name = "btnAlta"
+        Me.btnAlta.Size = New System.Drawing.Size(75, 23)
+        Me.btnAlta.TabIndex = 2
+        Me.btnAlta.Text = "Alta"
+        Me.btnAlta.UseVisualStyleBackColor = True
+        '
+        'btnBaja
+        '
+        Me.btnBaja.Location = New System.Drawing.Point(565, 180)
+        Me.btnBaja.Name = "btnBaja"
+        Me.btnBaja.Size = New System.Drawing.Size(75, 23)
+        Me.btnBaja.TabIndex = 3
+        Me.btnBaja.Text = "Baja"
+        Me.btnBaja.UseVisualStyleBackColor = True
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(565, 230)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.btnBuscar.TabIndex = 4
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(565, 290)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 5
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(36, 29)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(487, 284)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(36, 345)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 7
+        Me.lblNombre.Text = "Nombre"
+        '
+        'lblDireccion
+        '
+        Me.lblDireccion.AutoSize = True
+        Me.lblDireccion.Location = New System.Drawing.Point(36, 377)
+        Me.lblDireccion.Name = "lblDireccion"
+        Me.lblDireccion.Size = New System.Drawing.Size(52, 13)
+        Me.lblDireccion.TabIndex = 9
+        Me.lblDireccion.Text = "Dirección"
+        '
+        'lblTelefono
+        '
+        Me.lblTelefono.AutoSize = True
+        Me.lblTelefono.Location = New System.Drawing.Point(36, 420)
+        Me.lblTelefono.Name = "lblTelefono"
+        Me.lblTelefono.Size = New System.Drawing.Size(49, 13)
+        Me.lblTelefono.TabIndex = 10
+        Me.lblTelefono.Text = "Teléfono"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(170, 337)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 11
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(170, 377)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(100, 20)
+        Me.txtDireccion.TabIndex = 12
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(170, 420)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(100, 20)
+        Me.txtTelefono.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 493)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblTelefono)
+        Me.Controls.Add(Me.lblDireccion)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnBaja)
+        Me.Controls.Add(Me.btnAlta)
+        Me.Controls.Add(Me.btnListar)
+        Me.Controls.Add(Me.bntConectar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents bntConectar As Button
+    Friend WithEvents btnListar As Button
+    Friend WithEvents btnAlta As Button
+    Friend WithEvents btnBaja As Button
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblDireccion As Label
+    Friend WithEvents lblTelefono As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents txtTelefono As TextBox
+End Class
---
+++ DI/Segundo trimestre/GestionBaseDatosInicial/GestionBaseDatosInicial/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Public Class Form1
+    Dim oconexion As New OleDbConnection
+    Private Sub bntConectar_Click(sender As Object, e As EventArgs) Handles bntConectar.Click
+        oconexion.ConnectionString = ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\dbempresas.mdb;")
+        MessageBox.Show("connection sucsesfull")
+    End Sub
+
+    Private Sub btnListar_Click(sender As Object, e As EventArgs) Handles btnListar.Click
+        Dim ocomando = New OleDbCommand("Select * from tbempresas", oconexion)
+        Dim odatareader As OleDbDataReader
+        Dim dt As New DataTable
+
+        oconexion.Open()
+        odatareader = ocomando.ExecuteReader
+
+        dt.Load(odatareader)
+
+        DataGridView1.DataSource = dt
+        odatareader.Close()
+
+        oconexion.Close()
+    End Sub
+
+    Private Sub btnAlta_Click(sender As Object, e As EventArgs) Handles btnAlta.Click
+        Dim icomando = New OleDbCommand("INSERT INTO tbempresas(nombre,direccion,telefono) VALUES ('Fernando','general 3222', '89898')", oconexion)
+        oconexion.Open()
+        icomando.ExecuteNonQuery()
+        oconexion.Close()
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim bcomando = New OleDbCommand("Select * From tbempresas WHERE nombre='Fernando'", oconexion)
+        Dim odatareader As OleDbDataReader
+
+        oconexion.Open()
+        odatareader = bcomando.ExecuteReader
+        If (odatareader.Read()) Then
+            txtNombre.Text = odatareader("nombre")
+            txtDireccion.Text = odatareader("direccion")
+            txtTelefono.Text = odatareader("telefono")
+        Else
+            MessageBox.Show("El elemento buscado no existe")
+        End If
+        oconexion.Close()
+    End Sub
+
+    Private Sub btnBaja_Click(sender As Object, e As EventArgs) Handles btnBaja.Click
+        Dim dcomando = New OleDbCommand("DELETE FROM tbempresas WHERE nombre='Fernando'", oconexion)
+
+        oconexion.Open()
+        dcomando.ExecuteNonQuery()
+        oconexion.Close()
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Close()
+    End Sub
+End Class
