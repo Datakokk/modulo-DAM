39da5c8ff5203a77574eb3630d789be331747684
Datakokk <fiodorelzarster@gmx.com>
2025-01-26 19:40:20 +0100

    SGE: se ha finalizado el Move2one

--- SGE/2doTrimestre/soporte/models/models.py
+++ SGE/2doTrimestre/soporte/models/models.py
@@ -49,19 +49,40 @@ class incidencia(models.Model):
          default = fields.Date.context_today
      )
 
-     ubicacion = fields.Selection(
-         string='Ubicación',
-         selection=[
-             ('oficina', 'Oficina'),
-             ('almacen', 'Almacén'),
-             ('remoto', 'Remoto')
-         ]
-     )
-
+     ubicacion_id = fields.Many2one(
+        comodel_name = 'soporte.ubicacion',
+        string = 'Ubicación con Many2one',
+        ondelete = 'restrict',
+        required=True
+    )
+     
      cerrada = fields.Boolean(
          string = 'Cerrada'
      )
 
+class ubicacion(models.Model):
+    _name = 'soporte.ubicacion'
+    _description = 'Modelo para la gestión de ubicaciones'
+
+    nombre = fields.Char(
+        string = 'Nombre',
+        required = True
+    )
+
+    pabellon = fields.Selection(
+        string = 'Pabellón',
+        selection = [('1', 'Pabellón Paris'), ('2', 'Pabellón Roma'), ('3', 'Pabellón Londres')]
+    )
+
+    plana = fields.Selection(
+        string = 'Planta',
+        selection = [('0', 'Planta baja'), ('1', 'Planta primera'), ('2', 'Planta segunda')]
+    )
+
+    
+
+
+ 
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
@@ -72,3 +93,11 @@ class incidencia(models.Model):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
+# ubicacion = fields.Selection(
+#      string='Ubicación',
+#      selection=[
+#          ('oficina', 'Oficina'),
+#          ('almacen', 'Almacén'),
+#          ('remoto', 'Remoto')
+#      ]
+#  )
