4b1e57813799b390954d127fceed4be4912c5dc4
Datakokk <fiodorelzarster@gmx.com>
2025-01-31 16:35:56 +0100

    PMDM: Se ha empezado el ejercicio AnimacionAtlasMap

---
+++ PMDM/JuegosLibGDX/AnimacionAtlasMap/core/src/main/java/io/github/some_example_name/Main.java
@@ -0,0 +1,54 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private TextureAtlas atlas;
+    private float stateTime;
+    private Animation <TextureRegion> bigMarioAnimation;
+    private float x, y;
+    private Boolean isMoving;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        atlas = new TextureAtlas(Gdx.files.internal("Mario_and_Enemies.pack"));
+        TextureAtlas.AtlasRegion bigMarioRegion = atlas.findRegion("big_mario");
+        int count = bigMarioRegion.getRegionWidth() / 16;
+        Array<TextureRegion> allFrames = new Array<>();
+        System.out.println(count);
+        for (int i = 0; i < 3; i++){
+            allFrames.add(new TextureRegion(bigMarioRegion, i * 16, 0, 16, bigMarioRegion.getRegionHeight()));
+        }
+        bigMarioAnimation = new Animation<>(0.4f, allFrames);
+        x = 50;
+        y = 50;
+        isMoving = true;
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        stateTime += Gdx.graphics.getDeltaTime();
+        TextureRegion currentFrame = bigMarioAnimation.getKeyFrame(stateTime, true);
+        batch.draw(currentFrame, x, y, currentFrame.getRegionWidth()*3, currentFrame.getRegionHeight()*3);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
