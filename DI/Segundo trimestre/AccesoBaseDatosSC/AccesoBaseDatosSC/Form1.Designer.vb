6de42366aedae57c7d8084fe97e0c867e0050be3
Datakokk <fiodorelzarster@gmx.com>
2025-02-04 20:22:33 +0100

    DI:  Acceso a base de datos sin conexion finalizado

--- DI/Segundo trimestre/AccesoBaseDatosSC/AccesoBaseDatosSC/Form1.Designer.vb
+++ DI/Segundo trimestre/AccesoBaseDatosSC/AccesoBaseDatosSC/Form1.Designer.vb
@@ -22,29 +22,131 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.btnVer = New System.Windows.Forms.Button()
+        Me.lstNombre = New System.Windows.Forms.ListBox()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblDireccion = New System.Windows.Forms.Label()
+        Me.lblTelefono = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.btnInsertar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnConectar
         '
-        Me.Button1.Location = New System.Drawing.Point(386, 329)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnConectar.Location = New System.Drawing.Point(114, 342)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(75, 23)
+        Me.btnConectar.TabIndex = 0
+        Me.btnConectar.Text = "Conectar"
+        Me.btnConectar.UseVisualStyleBackColor = True
+        '
+        'btnVer
+        '
+        Me.btnVer.Location = New System.Drawing.Point(248, 342)
+        Me.btnVer.Name = "btnVer"
+        Me.btnVer.Size = New System.Drawing.Size(75, 23)
+        Me.btnVer.TabIndex = 1
+        Me.btnVer.Text = "Ver"
+        Me.btnVer.UseVisualStyleBackColor = True
+        '
+        'lstNombre
+        '
+        Me.lstNombre.FormattingEnabled = True
+        Me.lstNombre.Location = New System.Drawing.Point(114, 89)
+        Me.lstNombre.Name = "lstNombre"
+        Me.lstNombre.Size = New System.Drawing.Size(120, 186)
+        Me.lstNombre.TabIndex = 3
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(467, 89)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 4
+        Me.lblNombre.Text = "Nombre"
+        '
+        'lblDireccion
+        '
+        Me.lblDireccion.AutoSize = True
+        Me.lblDireccion.Location = New System.Drawing.Point(467, 162)
+        Me.lblDireccion.Name = "lblDireccion"
+        Me.lblDireccion.Size = New System.Drawing.Size(52, 13)
+        Me.lblDireccion.TabIndex = 5
+        Me.lblDireccion.Text = "Direccion"
+        '
+        'lblTelefono
+        '
+        Me.lblTelefono.AutoSize = True
+        Me.lblTelefono.Location = New System.Drawing.Point(467, 241)
+        Me.lblTelefono.Name = "lblTelefono"
+        Me.lblTelefono.Size = New System.Drawing.Size(49, 13)
+        Me.lblTelefono.TabIndex = 6
+        Me.lblTelefono.Text = "Telefono"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(599, 89)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 7
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(599, 155)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(100, 20)
+        Me.txtDireccion.TabIndex = 8
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(599, 234)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(100, 20)
+        Me.txtTelefono.TabIndex = 9
+        '
+        'btnInsertar
+        '
+        Me.btnInsertar.Location = New System.Drawing.Point(426, 341)
+        Me.btnInsertar.Name = "btnInsertar"
+        Me.btnInsertar.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsertar.TabIndex = 10
+        Me.btnInsertar.Text = "Insertar"
+        Me.btnInsertar.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnInsertar)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblTelefono)
+        Me.Controls.Add(Me.lblDireccion)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.lstNombre)
+        Me.Controls.Add(Me.btnVer)
+        Me.Controls.Add(Me.btnConectar)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnConectar As Button
+    Friend WithEvents btnVer As Button
+    Friend WithEvents lstNombre As ListBox
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblDireccion As Label
+    Friend WithEvents lblTelefono As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents btnInsertar As Button
 End Class
--- DI/Segundo trimestre/AccesoBaseDatosSC/AccesoBaseDatosSC/Form1.vb
+++ DI/Segundo trimestre/AccesoBaseDatosSC/AccesoBaseDatosSC/Form1.vb
@@ -4,11 +4,13 @@ Public Class Form1
     Dim oconexion As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\dbempresas.mdb;")
     Dim odataadapter As New OleDbDataAdapter("SELECT * FROM tbempresas", oconexion)
     Dim odataset As New DataSet
+    Dim obuilder As New OleDbCommandBuilder(odataadapter)
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
         Try
             ' Abrir la conexión
             oconexion.Open()
+            odataadapter.Fill(odataset, "tb1")
             MsgBox("Conexión exitosa a la base de datos,  Ok!!", MsgBoxStyle.Information, "Estado de Conexión")
         Catch ex As OleDbException
             ' Manejo de errores específicos de OleDb
@@ -23,4 +25,50 @@ Public Class Form1
             End If
         End Try
     End Sub
+
+    Private Sub btnVer_Click(sender As Object, e As EventArgs) Handles btnVer.Click
+        Dim ofila As DataRow
+        lstNombre.Items.Clear()
+
+        For Each ofila In odataset.Tables("tb1").Rows
+            lstNombre.Items.Add(ofila.Item("Nombre"))
+        Next
+
+    End Sub
+
+    Private Sub btnInsertar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+        Dim ofila As DataRow
+        Dim enc As Boolean = False
+
+        ' Verificar si el nombre ya existe en la tabla
+        For Each fila As DataRow In odataset.Tables("tb1").Rows
+            If fila("Nombre").ToString() = txtNombre.Text Then
+                MessageBox.Show("El nombre ya existe, por favor pruebe con otro", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                enc = True
+                Exit For
+            End If
+        Next
+
+        ' Solo insertar si no hay duplicados
+        If Not enc Then
+            ofila = odataset.Tables("tb1").NewRow
+            ofila("Nombre") = txtNombre.Text
+            ofila("Direccion") = txtDireccion.Text
+            ofila("Telefono") = txtTelefono.Text
+
+            odataset.Tables("tb1").Rows.Add(ofila)
+
+            ' Intentar actualizar la base de datos
+            Try
+                oconexion.Open()
+                odataadapter.Update(odataset, "tb1")
+                oconexion.Close()
+
+                MessageBox.Show("Se ha insertado correctamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Error al insertar los datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
 End Class
