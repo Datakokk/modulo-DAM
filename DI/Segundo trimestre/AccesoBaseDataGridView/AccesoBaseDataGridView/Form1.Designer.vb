68c478576c69fddd558a89b0b137e587f24d2b22
Datakokk <fiodorelzarster@gmx.com>
2025-02-12 20:57:14 +0100

    DI: Estamos con el ejercicio de DataGridView

---
+++ DI/Segundo trimestre/AccesoBaseDataGridView/AccesoBaseDataGridView/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblDireccion = New System.Windows.Forms.Label()
+        Me.lblTelefono = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.btnVer = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(518, 100)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 5
+        Me.lblNombre.Text = "Nombre"
+        '
+        'lblDireccion
+        '
+        Me.lblDireccion.AutoSize = True
+        Me.lblDireccion.Location = New System.Drawing.Point(518, 168)
+        Me.lblDireccion.Name = "lblDireccion"
+        Me.lblDireccion.Size = New System.Drawing.Size(52, 13)
+        Me.lblDireccion.TabIndex = 6
+        Me.lblDireccion.Text = "Direccion"
+        '
+        'lblTelefono
+        '
+        Me.lblTelefono.AutoSize = True
+        Me.lblTelefono.Location = New System.Drawing.Point(518, 238)
+        Me.lblTelefono.Name = "lblTelefono"
+        Me.lblTelefono.Size = New System.Drawing.Size(49, 13)
+        Me.lblTelefono.TabIndex = 7
+        Me.lblTelefono.Text = "Telefono"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(620, 93)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 8
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(620, 161)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(100, 20)
+        Me.txtDireccion.TabIndex = 9
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(620, 238)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(100, 20)
+        Me.txtTelefono.TabIndex = 10
+        '
+        'btnVer
+        '
+        Me.btnVer.Location = New System.Drawing.Point(553, 357)
+        Me.btnVer.Name = "btnVer"
+        Me.btnVer.Size = New System.Drawing.Size(75, 23)
+        Me.btnVer.TabIndex = 11
+        Me.btnVer.Text = "Ver"
+        Me.btnVer.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 56)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(454, 263)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'btnConectar
+        '
+        Me.btnConectar.Location = New System.Drawing.Point(146, 357)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(75, 23)
+        Me.btnConectar.TabIndex = 13
+        Me.btnConectar.Text = "Conectar"
+        Me.btnConectar.UseVisualStyleBackColor = True
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Location = New System.Drawing.Point(279, 357)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCargar.TabIndex = 14
+        Me.btnCargar.Text = "Cargar"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(414, 357)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 15
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnCargar)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnVer)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.lblTelefono)
+        Me.Controls.Add(Me.lblDireccion)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblDireccion As Label
+    Friend WithEvents lblTelefono As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents btnVer As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnConectar As Button
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents btnGuardar As Button
+End Class
---
+++ DI/Segundo trimestre/AccesoBaseDataGridView/AccesoBaseDataGridView/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+Public Class Form1
+    Dim oconexion As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\dbempresas.mdb;")
+    Dim oadapter As New OleDbDataAdapter("SELECT * FROM dbempresas", oconexion)
+    Dim odataset As New DataSet()
+    Dim obuilder As New OleDbCommandBuilder(oadapter) 'Importante si queremos modificar /borrar/insertar/ datos
+    Dim ofila As DataRow
+
+    'Se realiza la conexion
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        Try
+            oconexion.Open()
+            oadapter.Fill(odataset, "tbAuxiliar")
+            oconexion.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+        DataGridView1.ClearSelection() 'Limpiamos el DatagridView para que no se repita al darle varias veces o se bloquee el boton'
+        DataGridView1.DataSource = odataset 'Asignamos de donde extraemos los datos (odataset)
+        DataGridView1.DataMember = "tbAuxiliar"
+    End Sub
+
+    'Boton para cargarlo en el DatagridView
+
+End Class
